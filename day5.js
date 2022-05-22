c3a77cc718adb36a680515871f522a490adb4106
Nam-Wijeong <dnlwjdv@hanmail.net>
2022-05-22 19:19:09 +0900

    0522 update

---
+++ day5.js
@@ -0,0 +1,17 @@
+// 하루에 함수 하나씩 냅다 외우기~!! day 5
+// 삼육구 게임
+function 삼육구(number) {
+    let nums = number.toString().split('');
+    let clap = [];
+
+    for(let num of nums) {
+        if(num === '3' || num === '6' || num === '9') {
+            clap.push('짝👏');
+        }
+    }
+    return clap;
+}
+
+console.log(삼육구(369));
+console.log(삼육구(336699));
+console.log(삼육구(12));
