72bd21f21c1a63c42d1f322d1b5cb172845c6076
Nam-Wijeong <dnlwjdv@hanmail.net>
2022-05-24 18:57:01 +0900

    별트리만들기

--- day7.js
+++ day7.js
@@ -1,7 +1,20 @@
 // 하루에 함수 하나씩 냅다 외우기~!! day 7
-// 별트리 만들기🎄
+// 별트리 만들기🎄 (제로초님 코드)
 
+// const level = 5;
+// for(let i = 1; i <= level; i++) {
+//     console.log(" ".repeat(level - i) + "*".repeat(i * 2 - 1));
+// }
+
+// 별트리 만들기🎄 (재현님 코드) 
 const level = 5;
-for(let i = 1; i <= level; i++) {
-    console.log(" ".repeat(level - i) + "*".repeat(i * 2 - 1));
+for(let i =1; i <= level; i++) {
+    let tree = "";
+    for(let k = 1; k <= level - i; k++) {
+        tree += " ";
+    }
+    for(let j = 1; j <= i * 2 - 1; j++) {
+        tree += "*";
+    }
+    console.log(tree);
 }
